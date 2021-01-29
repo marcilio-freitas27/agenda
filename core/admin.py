d70d4a30541d3241223a04788980715ef8c85154
Marcilio Freitas <marbass18@gmail.com>
2021-01-29 19:56:58 -0300

    criando tabela de eventos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+# registrar suas classes e models
+
+#classe de administração
+class EventoAdmin(admin.ModelAdmin):
+    # campos para aparecer no django admin
+    list_display = ('titutlo','data_evento','data_criacao')
+    #filtro de titulos e demais conteudos
+    list_filter = ('usuario','data_evento',)
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#tabela
+
+class Evento(models.Model):
+    # campos da tabela tipo chracter, text e data
+    titutlo = models.CharField(max_length=100)
+    #pode ser em branco(blank) ou vazio(null)
+    descricao = models.TextField(blank=True,null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    # auto_now atualiza sempre para a hora atual
+    data_criacao = models.DateTimeField(auto_now=True)
+    # modifica uma classe para usar como delete através da foreign key
+    # on_delete = models.CASCADE para excluir os eventos do usuario
+    # escolher 1, valor 1 (admin), setar 1
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    #classe para nomear a tabela
+    class Meta:
+        db_table = 'evento'
+    
+    #função para nomear objeto do evento
+
+    def __str__(self):
+        return self.titutlo
+    
+    
